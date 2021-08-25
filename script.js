0b540d75156280025668fbef43e1da0feb12ebdf
Glitch (glitch-blank-website) <none>
2021-08-25 16:03:21 +0000

    🏞☃️ Checkpoint ./index.html:35377789/1295 ./script.js:35377789/1261 ./style.css:35377789/584

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+document.getElementById("Title").innerHTML = "<h2>Temple's Website</h2>";
 
-console.log("Hello, world!");
+
+document.getElementById("changeText").addEventListener("click", function () {
+  document.getElementById("myHeader").innerHTML = "Event Listeners are Cool!";
+});
+
+document.getElementById("myParagraph").style.textAlign = "center";
+document.getElementById("myParagraph").style.color = "lightblue";
+
+document.getElementById("sendAlert").addEventListener("click", function () {
+  alert("This is an alert that be used to notify the user");
+});
+
+document.getElementById("changeImage").addEventListener("click", function () {
+  document.getElementById("funImage").src = "https://secure.img1-fg.wfcdn.com/im/02238154/compr-r85/8470/84707680/pokemon-pikachu-wall-decal.jpg"
+});
