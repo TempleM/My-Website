539dd8e568327467dc731b84b36fdd7e9fd1fec2
Glitch (glitch-blank-website) <none>
2021-08-25 16:27:17 +0000

    🕍⚾️ Checkpoint ./index.html:35377789/175 ./style.css:35377789/316 ./script.js:35377789/128

--- script.js
+++ script.js
@@ -21,3 +21,5 @@ document.getElementById("sendAlert").addEventListener("click", function () {
 document.getElementById("changeImage").addEventListener("click", function () {
   document.getElementById("funImage").src = "https://secure.img1-fg.wfcdn.com/im/02238154/compr-r85/8470/84707680/pokemon-pikachu-wall-decal.jpg"
 });
+
+document.getElementsByClassName("button").style.textAlign = "center";
