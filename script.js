28a478f902ae9dcb4f0fe448fd8a6d3baaf0d241
Glitch (glitch-blank-website) <none>
2021-08-25 20:40:14 +0000

    ⛱🎮 Checkpoint ./.glitch-assets:35377789/768 ./script.js:35377789/1248 ./index.html:35377789/338 ./style.css:35377789/206

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ document.getElementById("Title").innerHTML = "<h2>Temple's Website</h2>";
 document.getElementById("Title").style.textAlign = "center";
 
 document.getElementById("aboutMe").addEventListener("click", function () {
-  alert("My name is Temple Miles");
+  alert("My name is Temple Miles. I am 15 years old and I was born on 11/03/05. I enjoy computer programming, video games, playing the piano, photography and more!");
 });
 document.getElementById("thisSite").addEventListener("click", function () {
-  alert("My name is Temple Miles");
+  alert("This is a website I made. Its a work in progress, so more will be added later. Right now it just has a few pictures I've taken over the past couple of years, as well as some info about me!");
 });
 document.getElementById("contactMe").addEventListener("click", function () {
-  alert("My name is Temple Miles");
+  alert("Phone #: 202-570-8029|Instagram: temple_miles1");
 });
 
 /*document.getElementById("myParagraph").style.textAlign = "center";*/
 document.getElementById("myParagraph").style.color = "white";
 
-document.getElementById("sendAlert").addEventListener("click", function () {
-  alert("This is an alert that be used to notify the user");
-});
-
-document.getElementById("changeImage").addEventListener("click", function () {
-  document.getElementById("funImage").src = "https://secure.img1-fg.wfcdn.com/im/02238154/compr-r85/8470/84707680/pokemon-pikachu-wall-decal.jpg"
-});
 
 document.getElementById("photo1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4133.jpg?v=1629910932269"
 document.getElementById("photo2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_3856.jpg?v=1629911746326"
@@ -47,9 +40,9 @@ document.getElementById("next").addEventListener("click", function (){
   
   document.getElementById("photo1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_0436.jpg?v=1629923167202"
   document.getElementById("photo2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_0423.jpg?v=1629923278628"
-  document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
+  document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_0415.jpg?v=1629923336633"
 
-  document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
+  document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4062.jpg?v=1629923386097"
   
   
 });
