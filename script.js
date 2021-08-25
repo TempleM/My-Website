85c38976e9bb5c59f92b0015105e515410be9e09
Glitch (glitch-blank-website) <none>
2021-08-25 19:42:11 +0000

    💏🌨 Checkpoint ./index.html:35377789/680 ./script.js:35377789/4666 ./style.css:35377789/22

--- script.js
+++ script.js
@@ -27,29 +27,6 @@ document.getElementById("photo2").src = "https://cdn.glitch.com/3dd3f809-d8e5-44
 document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4100.jpg?v=1629911912657"
 
 document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-document.getElementById("noPhoto2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-document.getElementById("noPhoto3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-document.getElementById("noPhoto4").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-document.getElementById("noPhoto5").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-document.getElementById("noPhoto6").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
-/*___________*/
-
-document.getElementById("first").addEventListener("click", function (){
-  
-  document.getElementById("photo1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4133.jpg?v=1629910932269"
-  document.getElementById("photo2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_3856.jpg?v=1629911746326"
-  document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4100.jpg?v=1629911912657"
-
-  document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto4").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto5").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto6").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
-
-});
 
 /*___________*/
 
@@ -60,12 +37,7 @@ document.getElementById("prev").addEventListener("click", function (){
   document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2FIMG_4100.jpg?v=1629911912657"
 
   document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto4").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto5").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto6").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
+  
   
 });
 
@@ -78,33 +50,8 @@ document.getElementById("next").addEventListener("click", function (){
   document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
 
   document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto4").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto5").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto6").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
-
   
-});
-
-/*___________*/
-
-document.getElementById("last").addEventListener("click", function (){
   
-  document.getElementById("photo1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("photo2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("photo3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
-  document.getElementById("noPhoto1").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto2").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto3").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto4").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto5").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-  document.getElementById("noPhoto6").src = "https://cdn.glitch.com/3dd3f809-d8e5-441a-ab9d-4ca413c5d577%2Fnoimage.png?v=1629913008298"
-
-
-
 });
 
 
